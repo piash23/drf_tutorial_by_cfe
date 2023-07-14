eb3113cc973ddb67e35f4def9fb2278f887654b1
Md. Shihab Uddin <pythonpiash@gmail.com>
2023-07-14 17:48:45 +0600

    fundamental echoing params and json data to requested api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse, JsonResponse
+import json
 
 
 def api_home(request):
-    return JsonResponse({"message": "Hello, world. You're at the api index."})
+    # receive json data
+    body = request.body  # byte string of json data
+    data = {}
+    try:
+        data = json.loads(body)  # convert byte string to dictionary
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
+    # url query parameters catching in django
+    print(request.GET)
+
+    data['query_params'] = dict(request.GET)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,9 +3,9 @@ start_time = time.time()
 
 endpoints = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoints, json={"show_env": 1})
+get_response = requests.get(endpoints, params={"query": "hello world"} ,json={"show_env": 1})
 print(get_response.status_code)
-print(type(get_response.text))
-print(type(get_response.json()))
+print(get_response.text)
+print(get_response.json())
 
 print("--- %s seconds ---" % (time.time() - start_time))
