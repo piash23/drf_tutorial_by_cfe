e1992893580383f1372418cee32a89232318d731
Md. Shihab Uddin <pythonpiash@gmail.com>
2023-07-14 17:18:40 +0600

    json response basic static data

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,11 @@
 import requests, time
 start_time = time.time()
 
-endpoints = "https://httpbin.org/anything"
+endpoints = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoints, json={"show_env": 1})
 print(get_response.status_code)
-print(get_response.text)
-print(get_response.json())
+print(type(get_response.text))
+print(type(get_response.json()))
 
 print("--- %s seconds ---" % (time.time() - start_time))
