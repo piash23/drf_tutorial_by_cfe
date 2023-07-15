c501f6b7b55705a0fa7de0f589986a9732db1174
Md. Shihab Uddin <pythonpiash@gmail.com>
2023-07-15 13:53:21 +0600

    added serializer and a custom serializer field

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 import json
 from products.models import Product
+from products.serializers import ProductSerializer
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -8,10 +9,10 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def api_home(request):
-    model_data = Product.objects.all().order_by('?').first()
+    instance = Product.objects.all().order_by('?').first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['title', 'content', 'price'])
+    if instance:
+        data = ProductSerializer(instance).data
 
     print(data)
     return Response(data)
