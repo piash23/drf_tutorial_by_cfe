96ac874b84c9c3a5054e090a14589425017c5c24
Md. Shihab Uddin <pythonpiash@gmail.com>
2023-07-15 13:24:22 +0600

    rendered rest framework api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse, JsonResponse
 import json
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(['GET'])
 def api_home(request):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
@@ -10,6 +14,6 @@ def api_home(request):
         data = model_to_dict(model_data, fields=['title', 'content', 'price'])
 
     print(data)
-    return JsonResponse(data)
+    return Response(data)
     # json_data_str = json.dumps(data)
     # return HttpResponse(json_data_str, headers={'Content-Type': 'application/json'})
